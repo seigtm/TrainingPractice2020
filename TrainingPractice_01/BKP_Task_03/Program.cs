731f7f7b2cb9cb174b6d2ea1aee6c4b62aa5c9e4
seigtm <seigtm@gmail.com>
2020-11-16 13:55:30 +0300

    Попытка выполнения третьего задания по практике.

---
+++ TrainingPractice_01/BKP_Task_03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BKP_Task_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string password = "admin";
+
+            for (int i = 0; i < 3; ++i)
+            {
+                Console.WriteLine("Введите пароль: ");
+                string userpassword = Console.ReadLine();
+
+                if (password == userpassword)
+                {
+                    Console.WriteLine("Успешный вход!");
+                    break;
+                }
+            }
+        }
+    }
+}
