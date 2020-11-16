c05ce236749f1ee171b3a1a1db4aaa08ddb0041c
seigtm <seigtm@gmail.com>
2020-11-16 13:42:58 +0300

    Попытка выполнения второго задания по практике.

---
+++ TrainingPractice_01/BKP_Task_02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BKP_Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (Console.ReadLine() != "exit") ;
+        }
+    }
+}
