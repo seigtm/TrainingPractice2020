988d1cd1f22b253cc77e93f0e9a52cf192f695ff
seigtm <seigtm@gmail.com>
2020-11-25 16:45:31 +0300

    Дикий говнокод вышел, конечно, но вроде работает.

--- TrainingPractice_02/EratosthenesSieve/Form1.Designer.cs
+++ TrainingPractice_02/EratosthenesSieve/Form1.Designer.cs
@@ -28,24 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle17 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle18 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle19 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle20 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.labelCurrentActive = new System.Windows.Forms.Label();
+            this.labelStep = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonCalculate.Location = new System.Drawing.Point(211, 390);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(75, 23);
+            this.buttonCalculate.TabIndex = 1;
+            this.buttonCalculate.Text = "Вычислить";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.ButtonCalculate_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBox1.Location = new System.Drawing.Point(105, 392);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            dataGridViewCellStyle17.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle17;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle18.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle18.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle18.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle18.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle18.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle18.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle18.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle18;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            dataGridViewCellStyle19.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle19.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle19.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle19.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle19.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle19.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle19.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle19;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            dataGridViewCellStyle20.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle20.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle20.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle20.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle20.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle20.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle20.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle20;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.ShowEditingIcon = false;
+            this.dataGridView1.Size = new System.Drawing.Size(548, 372);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(566, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(222, 371);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // labelCurrentActive
+            // 
+            this.labelCurrentActive.AutoSize = true;
+            this.labelCurrentActive.Location = new System.Drawing.Point(311, 395);
+            this.labelCurrentActive.Name = "labelCurrentActive";
+            this.labelCurrentActive.Size = new System.Drawing.Size(124, 13);
+            this.labelCurrentActive.TabIndex = 4;
+            this.labelCurrentActive.Text = "Нажмите \"вычислить\"!";
+            // 
+            // labelStep
+            // 
+            this.labelStep.AutoSize = true;
+            this.labelStep.Location = new System.Drawing.Point(12, 395);
+            this.labelStep.Name = "labelStep";
+            this.labelStep.Size = new System.Drawing.Size(47, 13);
+            this.labelStep.TabIndex = 4;
+            this.labelStep.Text = "Шаг №0";
             // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Controls.Add(this.labelStep);
+            this.Controls.Add(this.labelCurrentActive);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.dataGridView1);
             this.MaximizeBox = false;
             this.Name = "mainForm";
             this.ShowIcon = false;
-            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
             this.Text = "Решето Эратосфена";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label labelCurrentActive;
+        private System.Windows.Forms.Label labelStep;
     }
 }
 
--- TrainingPractice_02/EratosthenesSieve/Form1.cs
+++ TrainingPractice_02/EratosthenesSieve/Form1.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace EratosthenesSieve
 {
@@ -15,6 +9,86 @@ namespace EratosthenesSieve
         public mainForm()
         {
             InitializeComponent();
+        }
+
+        // Заполнение таблицы числами [2..lastNumber].
+        private void FillTable(uint lastNumber)
+        {
+            uint currentNumber = 2;
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = currentNumber++;
+                    if (currentNumber > lastNumber)
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+
+        // Визуализация вычёркивания простых чисел в таблице.
+        private void ColorNotPrimary(uint n)
+        {
+            uint step = 1; // Шаг цикла.
+            for (int number = 2; number < Math.Sqrt(n) + 1; number++)
+            {
+                for (int i = 0; i < dataGridView1.RowCount; i++)
+                {
+                    for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                    {
+                        int currentNumber = Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value);
+                        if (currentNumber % number == 0 && currentNumber != number)
+                        {
+                            // Выделяем серым кратные числа.
+                            dataGridView1.Rows[i].Cells[j].Style.BackColor = System.Drawing.Color.Gray;
+                        }
+                    }
+                }
+                // Обновляем данные о текущей итерации.
+                labelStep.Text = "Шаг №" + step++.ToString();
+                labelCurrentActive.Text = "Вычёркиваем кратные " + number.ToString();
+                Refresh();          // Обновляем интерфейс.
+                Thread.Sleep(2000); // Задержка на 2 секунды.
+            }
+            labelStep.Text = "Ушло шагов: " + step.ToString();
+            labelCurrentActive.Text = "Остались только простые числа!";
+        }
+
+        // Нажатие на кнопку "Вычислить".
+        private void ButtonCalculate_Click(object sender, EventArgs e)
+        {
+            // Очищаем таблицу с каждым нажатием кнопки.
+            dataGridView1.Rows.Clear();
+
+            // Проверяем, является ли введённая строка беззнаковым целым числом.
+            if (!uint.TryParse(textBox1.Text, out uint n)) // n - конечное число диапазона.
+            {
+                MessageBox.Show("Введите целое число!");
+                return;
+            }
+
+            // rows, columns - ряды и колонки таблицы.
+            // Вычисляются как корень квадратный из n.
+            int rows = Convert.ToInt32(Math.Sqrt(n));
+            int columns = Convert.ToInt32(Math.Sqrt(n));
+            // Если корень извлекается не целым.
+            // То нам необходимо нарисовать на один ряд больше,
+            //  чтобы поместить все числа в таблице.
+            if (rows - Math.Sqrt(n - 1) < 0) // n - 1, т.к. диапазон у нас - [2...n].
+            {
+                rows++;
+            }
+            // Устанавливаем количество рядов и колонок таблицы тут.
+            dataGridView1.RowCount = rows;
+            dataGridView1.ColumnCount = columns;
+
+            // Заполняем таблицу числами от 2 до n.
+            FillTable(n);
+
+            // Визуализируем алгоритма удаления кратных чисел.
+            ColorNotPrimary(n);
         }
     }
 }
